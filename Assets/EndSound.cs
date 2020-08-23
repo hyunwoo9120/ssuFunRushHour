a06858b29e32693773abd02b0de1cffdd3c03e23
Hyunwoo <hyunwoo.k9120@gmail.com>
2020-08-23 18:58:38 +0900

    RushHour

---
+++ Assets/EndSound.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndSound : MonoBehaviour
+{
+    int[] level = new int[4] { 1, 2, 3, 4 };
+    static int stage = 1;
+    Vector3 pos;
+    public AudioSource endSound;
+    bool playOnce = false;
+
+    private void Start()
+    {
+
+        endSound = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        if (CheckDestination(playOnce))
+        {
+            endSound.Play();
+            playOnce = true;
+            Invoke("ChangeScene", 3);
+        }
+    }
+
+    private bool CheckDestination(bool playOnce)
+    {
+        if (playOnce) return false;
+        pos = this.gameObject.transform.position;
+        if ((pos.x >= -1.1 && pos.x <= -0.9) && (pos.z >= 5.9 && pos.z <= 8.1))
+        {
+            print("도착");
+            return true;
+        }
+        else return false;
+    }
+
+    private void ChangeScene()
+    {
+        SceneManager.LoadScene("Level" + level[stage++]);
+        if (stage == 5) { EndGame(); }
+    }
+
+    private void EndGame() {
+        Application.Quit();
+    }
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private GameObject target;
+
+    void Start()
+    {
+        print("start");
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            target = GetClickedObject();
+                   }
+        if (target!=null && target.Equals(gameObject))  //선택된게 나라면
+        {
+            MoveBacknForth(target);
+        }
+    }
+
+    private GameObject GetClickedObject()
+    {
+        RaycastHit hit;
+        GameObject target = null;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (true == (Physics.Raycast(ray.origin, ray.direction * 12, out hit)))
+        {
+            target = hit.collider.gameObject;
+        }
+        return target;
+    }
+    void MoveBacknForth(GameObject target)
+    {
+        print("3. 앞뒤 움직임 시작");
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            target.transform.Translate(Vector3.right*2);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            target.transform.Translate(Vector3.left*2);
+        }
+
+        print("4. 앞뒤 움직임 완료");
+    }
+}
